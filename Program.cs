21b41590a31b4a3aeb92711f13ab452d1b049552
Arseniy Zorin <arsenchozzz@hotmail.com>
2020-08-26 11:15:49 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Toolkit.Parsers.Rss;
+using System;
+using System.Net.Http;
+
+namespace RSSParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParseRSS();
+            Console.ReadKey();
+        }
+
+        public static async void ParseRSS()
+        {
+            string feed = null;
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    feed = await client.GetStringAsync("https://news.yandex.ru/internet.rss");
+                }
+                catch { }
+            }
+
+            if (feed != null)
+            {
+                var parser = new RssParser();
+                var rss = parser.Parse(feed);
+
+                foreach (var element in rss)
+                {
+                    Console.WriteLine($"Заголовок: {element.Title}");
+                    Console.WriteLine($"Текст: {element.Summary}");
+                    Console.WriteLine($"Ссылка: {element.FeedUrl}");
+                }
+            }
+        }
+    }
+}
